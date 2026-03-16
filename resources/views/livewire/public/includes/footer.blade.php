1d1486a941b7351e5e7a532b7011d10baf1912cd
Saumy Anand <samcool3203@gmail.com>
2026-03-16 14:24:49 +0530

    footer image updated

--- resources/views/livewire/public/includes/footer.blade.php
+++ resources/views/livewire/public/includes/footer.blade.php
@@ -2,21 +2,14 @@
     <div class="container">
         <div class="row mb-2 md-mb-4 overlap-section" data-anime='{ "el": "childs", "translateY": [-15, 0], "scale": [0.5, 1], "opacity": [0,1], "duration": 800, "delay": 300, "staggervalue": 300, "easing": "easeOutQuad" }'>
             <div class="col-12 text-center">
-                <img class="rounded-circle" src="{{ asset('asset/images/demo-travel-agency-home-16.png') }}" alt="" />
+                <img class="rounded-circle" src="{{ asset('footer.png') }}" alt="" style="height: 150px;" />
             </div>
         </div>
         <!-- start subscribe item -->
         <div class="row justify-content-center mb-6 md-mb-8 xs-mb-40px">
             <div class="col-xl-6 col-lg-8 col-md-10 text-center last-paragraph-no-margin">
                 <h2 class="text-dark-gray alt-font fw-600 mb-40px mx-auto w-90 ls-minus-2px">Get the amazing travel offers into your inbox!</h2>
-                <div class="d-inline-block w-100 newsletter-style-03 position-relative mb-30px">
-                    <form action="email-templates/subscribe-newsletter.php" method="post" class="position-relative w-100">
-                        <input class="input-large bg-white border-color-transparent w-100 border-radius-100px box-shadow-extra-large form-control required" type="email" name="email" placeholder="Enter your email address" />
-                        <input type="hidden" name="redirect" value="">
-                        <button class="btn btn-large text-dark-gray ls-0px left-icon submit fw-600" aria-label="submit"><i class="icon feather icon-feather-mail icon-small text-base-color"></i><span>Subscribe</span></button>
-                        <div class="form-results border-radius-100px pt-10px pb-10px ps-15px pe-15px fs-16 mt-10px w-100 z-index-1 text-center position-absolute d-none"></div>
-                    </form>
-                </div>
+              
                 <p class="fs-16 lh-normal">We are committed to protecting your <a href="#" class="text-decoration-line-bottom text-dark-gray text-dark-gray-hover">privacy policy.</a></p>
             </div>
         </div>
@@ -24,7 +17,7 @@
      
         <div class="row align-items-center">
             <div class="col-xl-3 col-sm-6 text-center text-sm-start last-paragraph-no-margin fs-15 text-dark-gray order-3 order-md-1">
-                <p>&COPY; Copyright 2025 <a href="{{route('home')}}" target="_blank" class="text-dark-gray text-dark-gray-hover text-decoration-line-bottom fw-600">TeerthYatraHoliday</a></p>
+                <p>&COPY; Copyright 2026 <a href="{{route('home')}}" target="_blank" class="text-dark-gray text-dark-gray-hover text-decoration-line-bottom fw-600">TeerthYatraHoliday</a></p>
             </div>
             <div class="col-xl-6 text-center lg-mt-10px sm-mt-0 sm-mb-15px order-1 order-xl-2 order-md-3">
                 <ul class="footer-navbar">
